4f5475889d3dae21a8d163b7a3692bc6697c9d07
Eduardo Shanahan <contact@eduardoshanahan.com>
2014-11-28 14:45:31 +0000

    Added mySQL

---
+++ modules/mysql/mysql.py
@@ -0,0 +1,34 @@
+from fabric.api import task
+from fabric.api import sudo
+import tools
+
+
+default_password = 'MySuperPassword'
+
+@task
+def install():
+    """
+    mySQL from package
+    """
+    sudo('apt-get install -y mysql-server')
+
+
+@task
+def default_password():
+    """
+    Let the installer work without asking for root details
+    """
+    sudo('apt-get install debconf-utils')
+    password = 'mysql-server mysql-server/root_password password {0}'.format(default_password)
+    sudo("debconf-set-selections <<< '{0}'".format(password))
+    password_again = 'mysql-server mysql-server/root_password_again password {0}'.format(default_password)
+    sudo("debconf-set-selections <<< '{0}'".format(password_again))
+
+
+@task
+def install_default():
+    """
+    Install using the default credentials
+    """
+    default_password()
+    install()
