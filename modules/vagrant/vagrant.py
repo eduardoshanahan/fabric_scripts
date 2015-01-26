4ab1fbae9c62b0c8df5369e3e421640961820aa4
Eduardo Shanahan <contact@eduardoshanahan.com>
2015-01-26 23:56:58 +0000

    Dealing with multiple machines

--- modules/vagrant/vagrant.py
+++ modules/vagrant/vagrant.py
@@ -3,26 +3,30 @@ from fabric.api import local
 from fabric.api import task
 
 
-
-def set_env():
+def set_env(hosted):
     """
     Connect to Vagrant from the host machine
     """
     env.user = 'vagrant'
-    result = dict(line.split() for line in local(
-        'vagrant ssh-config', capture=True).splitlines())
+    config_details = local('vagrant ssh-config', capture=True).splitlines()
+    hosting = [line for line in config_details if 'Host ' in line or 'Port' in line]
+    print('Hosts available are',hosting)
+    result = dict((line.split() for line in config_details if line != ''))
     env.hosts = ['{0}:{1}'.format(result['HostName'], result['Port'])]
     env.key_filename = result['IdentityFile'].replace('"', '')
+    if hosted != '':
+        env.hosts = hosted
+    print('Going to connect to host \n{0}\n'.format(env.hosts))
     return env
 
 
 @task
-def connect():
+def connect(hosted=''):
     """
-    Apply Vagrant credentials to the SSH session
+    Apply Vagrant credentials to the SSH session (you can put :hosted=127.0.0.1:port_number)
     """
     from .. import ubuntu
-    env_details = set_env()
+    env_details = set_env(hosted)
     ubuntu.ubuntu.init(env_details)
 
 
@@ -40,11 +44,3 @@ def cleanup():
     Remove invalid machines
     """
     local('vagrant global-status --prune')
-
-
-@task
-def destroy(id):
-    """
-    Remove a machine -> :a3d345f or :id=a3d345f
-    """
-    local('vagrant destroy {0}'.format(id))
