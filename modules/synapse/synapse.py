6cbb3a24328cb08bef7ad5cd22de6f9517df1d43
Eduardo Shanahan <contact@eduardoshanahan.com>
2015-05-18 13:01:33 +0100

    Added synapse

---
+++ modules/synapse/synapse.py
@@ -0,0 +1,15 @@
+from fabric.api import cd
+from fabric.api import task
+from fabric.api import run
+from fabric.api import sudo
+from fabric.contrib.files import sed
+
+
+@task
+def install():
+    """
+    Get Redis source and build
+    """
+    sudo('apt-add-repository ppa:synapse-core/testing')
+    sudo('apt-get update')
+    sudo(apt-get install synapse)
