2a197aae566d828a8ec02ce8ea2dbb5fbd46dc51
Eduardo Shanahan <contact@eduardoshanahan.com>
2015-07-30 13:41:20 +0100

    Fixes in Tomcat

--- modules/java/tomcat.py
+++ modules/java/tomcat.py
@@ -9,10 +9,10 @@ def install():
     """
     Install Tomcat 7
     """
-    append('/etc/environment', 'JAVA_HOME="/usr/bin/java"', use_sudo=True)
+    # append('/etc/environment', 'JAVA_HOME="/usr/bin/java"', use_sudo=True)
     sudo('apt-get install -y tomcat7')
 
-# 
+#
 # @task
 # def tools():
 #     """
